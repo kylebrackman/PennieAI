38d85cf376ec863dd9a3ceafe5010129216fe286
Kyle Brackman <kyle.a.brackman@gmail.com>
2025-09-26 17:37:49 -0600

    create user and patient structs

--- models/patient.go
+++ models/patient.go
@@ -1,1 +1,19 @@
 package models
+
+import (
+	"time"
+)
+
+type Patient struct {
+	ID          int        `json:"id" db:"id"`
+	Name        string     `json:"name" db:"name"`
+	Species     *string    `json:"species" db:"species"`
+	Breed       *string    `json:"breed" db:"breed"`
+	Sex         *string    `json:"sex" db:"sex"`
+	DateOfBirth *time.Time `json:"date_of_birth" db:"date_of_birth"`
+	Weight      *float64   `json:"weight" db:"weight"`
+	Height      *float64   `json:"height" db:"height"`
+	Color       *string    `json:"color" db:"color"`
+	CreatedAt   time.Time  `json:"created_at" db:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at" db:"updated_at"`
+}
--- models/user.go
+++ models/user.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"time"
+)
+
+type User struct {
+	ID           int        `json:"id" db:"id"`
+	FirebaseUID  string     `json:"firebase_uid" db:"firebase_uid"`
+	Email        string     `json:"email" db:"email"`
+	FirstName    *string    `json:"first_name" db:"first_name"`
+	LastName     *string    `json:"last_name" db:"last_name"`
+	LastSignInAt *time.Time `json:"last_sign_in_at" db:"last_sign_in_at"`
+	CreatedAt    time.Time  `json:"created_at" db:"created_at"`
+	UpdatedAt    time.Time  `json:"updated_at" db:"updated_at"`
+}
