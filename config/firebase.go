e3446a601536b05335a5fe030af08e5bedfbb252
Kyle Brackman <kyle.a.brackman@gmail.com>
2025-12-22 22:31:40 -0500

    docker compose

--- config/firebase.go
+++ config/firebase.go
@@ -3,15 +3,18 @@ package config
 import (
 	"context"
 	"log"
+	"os"
 
 	"firebase.google.com/go/v4"
+	"google.golang.org/api/option"
 )
 
 var FirebaseApp *firebase.App
 
 func InitFirebase() (*firebase.App, error) {
+	opt := option.WithCredentialsFile(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
 	var err error
-	FirebaseApp, err = firebase.NewApp(context.Background(), nil)
+	FirebaseApp, err = firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
